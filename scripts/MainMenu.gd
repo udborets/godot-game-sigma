4c0b5d4737e3a75c9306535120a5c67720c38c8b
Yury Borets <udborets@gmail.com>
2024-03-12 22:45:02 +0300

    add main menu node

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+
+
+func _on_play_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+
+func _on_updates_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	pass # Replace with function body.
