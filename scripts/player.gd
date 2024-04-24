4a02855c1686b5f567e86e279b01e659022a5f26
Mark Mozharovski <mark.mozh@gmail.com>
2024-04-24 21:13:06 +0200

    add camera shake

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,20 +10,23 @@ extends CharacterBody3D
 
 #SPEED VARS
 
-var current_speed: float = 5.0
+var current_speed: float = 4.0
 @export var walking_speed: float = 3.0
-@export var sprinting_speed: float = 8.0
+@export var sprinting_speed: float = 6.0
 @export var crouching_speed: float = 3.0
 
 #MOVEMENT VARS
 
 const JUMP_VELOCITY = 4.5
 
-var lerp_speed: float = 10.0
+var lerp_stop_speed: float = 50.0
+var lerp_head_speed: float = 10.0
 
 var crouching_depth: float = -0.5
 
 var direction: Vector3 = Vector3.ZERO
+
+var default_head_position: float
 
 #INPUT VARS
 
@@ -34,6 +37,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	default_head_position = head.position.y
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -47,7 +51,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("crouch"):
 		#crouching
 		
-		head.position.y = lerp(head.position.y, 0.6 + crouching_depth, delta * lerp_speed)
+		head.position.y = lerp(head.position.y, 0.6 + crouching_depth, delta * lerp_head_speed)
 		current_speed = crouching_speed
 		standing_shape.disabled = true
 		crouching_shape.disabled = false
@@ -56,7 +60,7 @@ func _physics_process(delta):
 		#standing
 		standing_shape.disabled = false
 		crouching_shape.disabled = true
-		head.position.y = lerp(head.position.y, 0.6, delta * lerp_speed)
+		head.position.y = lerp(head.position.y, 0.6, delta * lerp_head_speed)
 		
 		#HANDLE SPRINTING
 		if Input.is_action_pressed("sprint"):
@@ -76,11 +80,14 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
-	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta * lerp_speed)
+	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_stop_speed)
+	print(direction)
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
+		head.position.y = lerp(head.position.y, default_head_position + 0.1 * sin(Time.get_ticks_msec() / 70), delta * lerp_head_speed)
 	else:
+		head.position.y = default_head_position
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
 
