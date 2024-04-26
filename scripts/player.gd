abe7af5a232bdae2a250275c25612c35c8509fb5
Mark Mozharovski <mark.mozh@gmail.com>
2024-04-26 15:56:38 +0200

    add balunboy, dobryak and skromnyaga models

--- scripts/player.gd
+++ scripts/player.gd
@@ -21,8 +21,8 @@ const JUMP_VELOCITY = 4.5
 
 var lerp_stop_speed: float = 50.0
 var lerp_head_speed: float = 10.0
-var head_movement_walking: float = 0.05
-var head_freq_walking: float = 300
+var camera_shake: float = 0.05
+var camera_shake_freq: float = 300
 
 var crouching_depth: float = -0.5
 
@@ -75,7 +75,7 @@ func handle_movement(delta):
 		animation_player.play("Walk")
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
-		head.position.y = lerp(head.position.y, head.position.y + head_movement_walking * sin(current_speed / head_freq_walking * Time.get_ticks_msec()), delta * lerp_head_speed)
+		head.position.y = lerp(head.position.y, head.position.y + camera_shake * sin(current_speed / camera_shake_freq * Time.get_ticks_msec()), delta * lerp_head_speed)
 	else:
 		animation_player.play("HappyIdle")
 		velocity.x = move_toward(velocity.x, 0, current_speed)
